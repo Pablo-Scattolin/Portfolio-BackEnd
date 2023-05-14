a13a9302e88b65de008bb55ccfe13a1e6aa80cdd
Pablo-Scattolin <122050191+Pablo-Scattolin@users.noreply.github.com>
2023-05-14 18:28:15 -0300

    Update CExperiencia.java

--- src/main/java/com/portfolio/api/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/api/Controller/CExperiencia.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://fir-frontend-8ccc7.firebaseapp.com/portfolio")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
